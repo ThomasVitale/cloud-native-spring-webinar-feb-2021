3c384395ca026a665cbbe431c14452063b1addd8
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-02-09 22:45:43 +0100

    Add functions

--- greeting-function/src/main/java/com/thomasvitale/greetingfunction/GreetingFunctionApplication.java
+++ greeting-function/src/main/java/com/thomasvitale/greetingfunction/GreetingFunctionApplication.java
@@ -1,7 +1,12 @@
 package com.thomasvitale.greetingfunction;
 
+import java.util.function.Function;
+
+import reactor.core.publisher.Flux;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GreetingFunctionApplication {
@@ -10,4 +15,13 @@ public class GreetingFunctionApplication {
 		SpringApplication.run(GreetingFunctionApplication.class, args);
 	}
 
+	@Bean
+	public Function<String, String> uppercase() {
+		return String::toUpperCase;
+	}
+
+	@Bean
+	public Function<Flux<String>, Flux<String>> reverse() {
+		return greeting -> greeting.map(g -> new StringBuilder(g).reverse().toString());
+	}
 }
