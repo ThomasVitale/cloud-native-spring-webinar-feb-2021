6eaa7536269178473a50bb1f1b6f2b8572174acd
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-02-09 16:34:58 +0100

    Initial commit

---
+++ book-app/src/main/java/com/thomasvitale/bookapp/BookAppApplication.java
@@ -0,0 +1,12 @@
+package com.thomasvitale.bookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAppApplication.class, args);
+	}
+}
---
+++ book-app/src/test/java/com/thomasvitale/bookapp/BookAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.thomasvitale.bookapp;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
