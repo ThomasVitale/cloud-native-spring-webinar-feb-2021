f3102a469ec867d740b025210bbfc92672a30856
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-02-09 22:34:15 +0100

    Add function demo

---
+++ greeting-function/src/main/java/com/thomasvitale/greetingfunction/GreetingFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.thomasvitale.greetingfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingFunctionApplication.class, args);
+	}
+
+}
---
+++ greeting-function/src/test/java/com/thomasvitale/greetingfunction/GreetingFunctionApplicationTests.java
@@ -0,0 +1,14 @@
+package com.thomasvitale.greetingfunction;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
